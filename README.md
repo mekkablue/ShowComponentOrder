0a0dd0be99ae727c0535445f5f6d91086d37dc88
mekka blue <mekka@mekkablue.com>
2014-02-01 12:03:07 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# ShowComponentOrder.glyphsReporter
+
+This is a plugin for the [Glyphs font editor](http://glyphsapp.com/) by Georg Seifert.
+It displays components in different colors depending on the order of the component. This way, you can step through your component-based glyphs (fn-arrows or Home/End on large keyboards) and quickly spot an order mistake.
+After installation, it will add the menu item *View > Show Component Order*.
+You can set a keyboard shortcut in System Preferences.
+
+![Components are shown in different colors.](ShowComponentOrder.png "Show Component Order Screenshot")
+
+### Installation
+
+1. Download the complete ZIP file and unpack it, or clone the repository.
+2. Double click the .glyphsReporter file. Confirm the dialog that appears in Glyphs.
+3. Restart Glyphs
+
+### Usage Instructions
+
+1. Open a glyph in Edit View.
+2. Use *View > Show Component Order* to toggle the the Component Order View.
+
+### License
+
+Copyright 2014 Rainer Erich Scheichelbauer (@mekkablue).
+Based on sample code by Georg Seifert (@schriftgestalt).
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+See the License file included in this repository for further details.
---
+++ ShowComponentOrder.glyphsReporter/Contents/Resources/ShowComponentOrder.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+import objc
+from Foundation import *
+from AppKit import *
+import sys, os, re
+
+GlyphsReporterProtocol = objc.protocolNamed( "GlyphsReporter" )
+
+class ShowComponentOrder ( NSObject, GlyphsReporterProtocol ):
+	
+	def init( self ):
+		"""
+		Unless you know what you are doing, leave this at "return self".
+		"""
+		#Bundle = NSBundle.bundleForClass_( NSClassFromString( self.className() ));
+		NSBezierPath.setDefaultLineJoinStyle_( NSRoundLineJoinStyle )
+		return self
+		
+	def title( self ):
+		"""
+		This is the name as it appears in the menu in combination with 'Show'.
+		E.g. 'return "Nodes"' will make the menu item read "Show Nodes".
+		"""
+		return "Component Order"
+		
+	def interfaceVersion( self ):
+		"""
+		Must return 1.
+		"""
+		return 1
+		
+	def logToConsole( self, message ):
+		"""
+		The variable 'message' will be passed to Console.app.
+		Use self.logToConsole( "bla bla" ) for debugging.
+		"""
+		myLog = "Show %s plugin:\n%s" % ( self.title(), message )
+		NSLog( myLog )
+		
+	def keyEquivalent( self ):
+		"""
+		The key for the keyboard shortcut. Set modifier keys in modifierMask() further below.
+		Pretty tricky to find a shortcut that is not taken yet, so be careful.
+		If you are not sure, use 'return None'. Users can set their own shortcuts in System Prefs.
+		"""
+		return "y"
+		
+	def modifierMask( self ):
+		"""
+		Use any combination of these to determine the modifier keys for your default shortcut:
+			return NSShiftKeyMask | NSControlKeyMask | NSCommandKeyMask | NSAlternateKeyMask
+		Or:
+			return 0
+		... if you do not want to set a shortcut.
+		"""
+		return 0
+	
+	def getScale( self ):
+		"""
+		Returns the current scale factor of the Edit View UI.
+		Divide any scalable size by this value in order to keep the same pixel size.
+		"""
+		try:
+			return self.controller.graphicView().scale()
+		except:
+			self.logToConsole( "Scale defaulting to 1.0" )
+			return 1.0
+	
+	def drawForegroundForLayer_( self, Layer ):
+		"""
+		Whatever you draw here will be displayed IN FRONT OF the paths.
+		Setting a color:
+			NSColor.colorWithCalibratedRed_green_blue_alpha_( 1.0, 1.0, 1.0, 1.0 ).set() # sets RGBA values between 0.0 and 1.0
+			NSColor.redColor().set() # predefined colors: blackColor, blueColor, brownColor, clearColor, cyanColor, darkGrayColor, grayColor, greenColor, lightGrayColor, magentaColor, orangeColor, purpleColor, redColor, whiteColor, yellowColor
+		Drawing a path:
+			myPath = NSBezierPath.alloc().init()  # initialize a path object myPath
+			myPath.appendBezierPath_( subpath )   # add subpath to myPath
+			myPath.fill()   # fill myPath with the current NSColor
+			myPath.stroke() # stroke myPath with the current NSColor
+		To get an NSBezierPath from a GSPath, use the bezierPath() method:
+			myPath.bezierPath().fill()
+		You can apply that to a full layer at once:
+			if len( myLayer.paths > 0 ):
+				myLayer.bezierPath()       # all closed paths
+				myLayer.openBezierPath()   # all open paths
+		See:
+		https://developer.apple.com/library/mac/documentation/Cocoa/Reference/ApplicationKit/Classes/NSBezierPath_Class/Reference/Reference.html
+		https://developer.apple.com/library/mac/documentation/cocoa/reference/applicationkit/classes/NSColor_Class/Reference/Reference.html
+		"""
+		try:
+			theComponents = Layer.components
+			numberOfComponents = len( theComponents )
+			
+			if numberOfComponents != 0:
+				factor = 1.0 / float( numberOfComponents )
+				
+				for i in range( numberOfComponents ):
+					difference = factor * float( i )
+					componentArea = theComponents[i].bezierPath()
+					
+					NSColor.colorWithCalibratedRed_green_blue_alpha_( difference ** 2.0, 1.0 - difference, difference, 1.0 ).set()
+					componentArea.fill()
+					
+					NSColor.blackColor().set()
+					componentArea.setLineWidth_( 3.0 / self.getScale() )
+					componentArea.stroke()
+					
+		except Exception as e:
+			self.logToConsole( str(e) )
+		
+	def drawBackgroundForLayer_( self, Layer ):
+		"""
+		Whatever you draw here will be displayed BEHIND the paths.
+		"""
+		try:
+			pass
+		except Exception as e:
+			self.logToConsole( str(e) )
+			
+	def drawBackgroundForInactiveLayer_( self, Layer ):
+		"""
+		Whatever you draw here will be displayed behind the paths, but for inactive masters.
+		"""
+		try:
+			pass
+		except Exception as e:
+			self.logToConsole( str(e) )
+			
+	def setController_( self, Controller ):
+		"""
+		Use self.controller as object for the current view controller.
+		"""
+		self.controller = Controller
